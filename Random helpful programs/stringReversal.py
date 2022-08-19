6a8296d444e4ed3589c6f473658730c160a9be3b
Brandon <btphill4@asu.edu>
2022-08-18 23:45:33 -0700

    initial commit

---
+++ Random helpful programs/stringReversal.py
@@ -0,0 +1,3 @@
+s = "}WzUxOTcsMTUyXQ.nlKMYTMJ6HH5aL8PNGvcIqt3deY{egelloc.nwp" # initial string
+reversed=''.join(reversed(s)) # .join() method merges all of the characters resulting from the reversed iteration into a new string
+print(reversed) #print the reversed string
